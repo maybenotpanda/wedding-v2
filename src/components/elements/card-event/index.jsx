136aac5c3bba55fd9f59991504b8e69c34a106ad
maybenotpanda <maybenotpanda@gmail.com>
2025-09-09 04:08:55 +0700

    feat-card-event: change color background

--- src/components/elements/card-event/index.jsx
+++ src/components/elements/card-event/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardEvent = (props) => {
 	return props.data.map((data, index) => (
-		<div key={index} className="p-4 bg-background rounded-3xl shadow-sm w-full relative bg-slate-50 text-center">
+		<div key={index} className="p-4 rounded-3xl shadow-sm w-full relative bg-background text-center">
 			<h5>{data.name}</h5>
 			<h5>{data.date}</h5>
 			<h5>{data.time}</h5>
