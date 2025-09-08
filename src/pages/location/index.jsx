786b0e40dde48c6889b84911a7b993f09f941672
maybenotpanda <maybenotpanda@gmail.com>
2025-09-09 04:09:25 +0700

    feat-location: changes assets flower to cloud

--- src/pages/location/index.jsx
+++ src/pages/location/index.jsx
@@ -1,40 +1,41 @@
-// import React from 'react'
+import React from 'react'
 
-// import flower1 from '../../assets/images/flower1.png'
-// import flower2 from '../../assets/images/flower2.png'
-// import flower3 from '../../assets/images/flower3.png'
-// import flower4 from '../../assets/images/flower4.png'
+import cloud from 'assets/images/cloud.png'
 
-// const LocationPage = () => {
-// 	return (
-// 		<div className="h-screen flex justify-center items-center bg-[#E1E2E2]">
-// 			<div className="grid gap-4 px-5">
-// 				<h4 className="text-center text-[#545454]">THE WEDDING OF</h4>
-// 				<h1 className="font-header text-center text-[#865D3D] text-5xl">Mw & Reza</h1>
-// 				<h4 className="text-center text-[#545454]">Lokasi Acara :</h4>
-// 				<div className="grid gap-5">
-// 					<a href="https://maps.app.goo.gl/FsAESiQC7XRW1Whe7" className="bg-[#0D2C46] text-white text-center py-2 static">
-// 						<h6>Akad</h6>
-// 					</a>
-// 					<a href="https://maps.app.goo.gl/WrvbPF5qC925nWaY9" className="bg-[#0D2C46] text-white text-center py-2 static">
-// 						<h6>Resepsi</h6>
-// 					</a>
-// 				</div>
-// 			</div>
-// 			<div className="image-left2">
-// 				<img className="bg-cover absolute bottom-0 left-0 w-20" src={flower3} alt="flower 3" />
-// 			</div>
-// 			<div className="image-right2">
-// 				<img className="bg-cover absolute bottom-0 right-0 w-20" src={flower4} alt="flower 4" />
-// 			</div>
-// 			<div className="image-left">
-// 				<img className="bg-cover absolute bottom-0 left-0 w-40" src={flower1} alt="flower 1" />
-// 			</div>
-// 			<div className="image-right">
-// 				<img className="bg-cover absolute bottom-0 right-0 w-48" src={flower2} alt="flower 2" />
-// 			</div>
-// 		</div>
-// 	)
-// }
+const LocationPage = () => {
+	return (
+		<div className="h-screen flex justify-center items-center bg-background">
+			<div className="grid gap-4 px-5">
+				<h4 className="text-center text-secondary">THE WEDDING OF</h4>
+				<h1 className="font-header text-center text-primary text-5xl">Mw & Reza</h1>
+				<h4 className="text-center text-[#545454]">Lokasi Acara :</h4>
+				<div className="grid gap-5">
+					<a
+						href="https://maps.app.goo.gl/FsAESiQC7XRW1Whe7"
+						className="bg-primary text-white  hover:bg-[#0D2C46] text-center py-2 static">
+						<h6>Akad</h6>
+					</a>
+					<a
+						href="https://maps.app.goo.gl/WrvbPF5qC925nWaY9"
+						className="bg-primary hover:bg-[#0D2C46] text-white text-center py-2 static">
+						<h6>Resepsi</h6>
+					</a>
+				</div>
+			</div>
+			<div className="image-left2">
+				<img className="bg-cover absolute top-0 left-0 w-20" src={cloud} alt="cloud" />
+			</div>
+			<div className="image-right2">
+				<img className="bg-cover absolute bottom-0 right-0 w-20" src={cloud} alt="cloud" />
+			</div>
+			<div className="image-left">
+				<img className="bg-cover absolute bottom-0 left-0 w-40" src={cloud} alt="cloud" />
+			</div>
+			<div className="image-right">
+				<img className="bg-cover absolute top-0 right-0 w-48" src={cloud} alt="cloud" />
+			</div>
+		</div>
+	)
+}
 
-// export default LocationPage
+export default LocationPage
