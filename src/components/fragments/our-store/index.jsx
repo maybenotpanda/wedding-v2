5a540cd97a4c87067dd459db068933572f05b532
maybenotpanda <maybenotpanda@gmail.com>
2025-09-12 18:30:10 +0700

    feat-out-store: update text

--- src/components/fragments/our-store/index.jsx
+++ src/components/fragments/our-store/index.jsx
@@ -1,18 +1,24 @@
 import Head from 'components/elements/head'
-import React, { Fragment } from 'react'
+import React from 'react'
 
 const OurStore = () => {
 	return (
-		<div>
-            <Head title="Our Story" />
-			<h6 className='text-center italic py-2 px-4 text-black'>
-				Sembilan tahun lalu, kami bertemu sebagai remaja di bangku SMA. <br />
-				Kami tumbuh bersama. berjalan berdampingan menapaki masa kuliah. <br />
-				berjuang dalam perkerjaan dan berkali-kali dipisahkan jarak. <br />
+		<div className='py-6'>
+			<Head title="Our Story" />
+			<h6 className="text-center italic py-2 px-4 text-black">
+				Semua bermula di bangku SMK kelas 2 pada tahun 2016. MW tak menyadari bahwa Fariz sebenarnya satu kelas dengannya.
+				Sebuah percakapan sederhana tentang sampul buku seni budaya berwarna hijau menjadi awal dari kisah panjang kami.
+				Dari sana, kami mulai berjalan beriringan, saling mengenal, dan tumbuh bersama.
 				<br />
-				Namun cinta tak pernah menyerah. Ia belajar sabar, bertahan, dan percaya. <br />
-				Hingga akhirnya kami tiba di titik ini. <br />
-				Satu ikatan suci, satu tujuan, satu doa yang akan dikabulkan-Nya.
+				<br />
+				Perjalanan kami tidak selalu mudah. Kami lebih sering dipisahkan oleh jarak dibandingkan berada di kota yang sama.
+				Dari Palembang-Bandung, Medan-Bandung, Medan-Jakarta, hingga Lampung-Jakarta, cinta ini diuji oleh waktu dan ruang.
+				Namun, setiap jarak justru semakin menguatkan langkah kami. hingga akhirnya mendarat di kota yang sama: Jakarta.
+				<br />
+				<br />
+				Setelah sembilan tahun perjalanan panjang yang penuh gelombang, kini kami memulai babak baru, berjanji seumur hidup
+				untuk berjalan bersama. Dengan ridho Allah SWT, dengan membangun keluarga Sakinah, mawaddah, dan rahmah, hingga
+				akhir hayat.
 			</h6>
 		</div>
 	)
