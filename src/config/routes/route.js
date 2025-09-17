4f876cea0e459186f48a1c55b4647772084ae32a
maybenotpanda <maybenotpanda@gmail.com>
2025-09-17 10:30:57 +0700

    feat-route: add new frame loading

--- src/config/routes/route.js
+++ src/config/routes/route.js
@@ -2,18 +2,18 @@ import React, { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LocationPage from '../../pages/location'
 import ThankYouPages from '../../pages/thank-you'
+import Loading from 'components/frame/loading'
 
 const HomePage = lazy(() => import('pages/home'))
-const Page404 = lazy(() => import('../../pages/404'))
 
 const RoutePage = () => {
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
+		<Suspense fallback={<Loading />}>
 			<Routes>
 				<Route path=":slug" element={<HomePage />} />
 				<Route path="/location" element={<LocationPage />} />
 				<Route path='/makasih' element={<ThankYouPages />} />
-				<Route path="*" element={<Page404 />} />
+				<Route path="*" element={<HomePage />} />
 			</Routes>
 		</Suspense>
 	)
