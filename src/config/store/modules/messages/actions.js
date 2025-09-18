c5a7fd4e6719374b7c3bc473ce714decadacc506
maybenotpanda <maybenotpanda@gmail.com>
2025-09-19 00:29:11 +0700

    feat-message: changes in selector name types & clean code with prettierrc

--- src/config/store/modules/messages/actions.js
+++ src/config/store/modules/messages/actions.js
@@ -10,37 +10,37 @@ import { failureAction, startAction, successAction } from 'config/utils/action'
 
 // ! create
 export const createMessage = (req) => {
-  return async (dispatch) => {
-    dispatch(startAction(ACTION_TYPES.MESSAGE_CREATE_START))
+	return async (dispatch) => {
+		dispatch(startAction(ACTION_TYPES.MESSAGE_CREATE_START))
 
-    try {
-      const res = await apiGeneral.request({
-        method: 'post',
-        url: `${API_URLS.CREATE_MESSAGE}`,
-        data: req
-      })
-      dispatch(successAction(ACTION_TYPES.MESSAGE_CREATE_SUCCESS, res))
-    } catch (err) {
-      const message = err || 'Unknown error occurred'
-      dispatch(failureAction(ACTION_TYPES.MESSAGE_CREATE_FAILED, message))
-    }
-  }
+		try {
+			const res = await apiGeneral.request({
+				method: 'post',
+				url: `${API_URLS.CREATE_MESSAGE}`,
+				data: req
+			})
+			dispatch(successAction(ACTION_TYPES.MESSAGE_CREATE_SUCCESS, res))
+		} catch (err) {
+			const message = err || 'Unknown error occurred'
+			dispatch(failureAction(ACTION_TYPES.MESSAGE_CREATE_FAILED, message))
+		}
+	}
 }
 
 // ! read
 export const listMessage = ({ page, limit }) => {
-  return async (dispatch) => {
-    dispatch(startAction(ACTION_TYPES.MESSAGE_LIST_START))
+	return async (dispatch) => {
+		dispatch(startAction(ACTION_TYPES.MESSAGE_LIST_START))
 
-    try {
-      const res = await apiGeneral.request({
-        method: 'get',
-        url: `${API_URLS.LIST_MESSAGE}?page=${page}&limit=${limit}`
-      })
-      dispatch(successAction(ACTION_TYPES.MESSAGE_LIST_SUCCESS, res.data.data))
-    } catch (err) {
-      const message = err.response?.data || 'Unknown error occurred'
-      dispatch(failureAction(ACTION_TYPES.MESSAGE_LIST_FAILED, message.message))
-    }
-  }
+		try {
+			const res = await apiGeneral.request({
+				method: 'get',
+				url: `${API_URLS.LIST_MESSAGE}?page=${page}&limit=${limit}`
+			})
+			dispatch(successAction(ACTION_TYPES.MESSAGE_LIST_SUCCESS, res.data.data))
+		} catch (err) {
+			const message = err.response?.data || 'Unknown error occurred'
+			dispatch(failureAction(ACTION_TYPES.MESSAGE_LIST_FAILED, message.message))
+		}
+	}
 }
--- src/config/store/modules/messages/reducer.js
+++ src/config/store/modules/messages/reducer.js
@@ -2,33 +2,33 @@
 import ACTION_TYPES from 'config/enums/types'
 
 const INITIAL_STATE = {
-  create: {},
-  list: {},
-  isLoading: false,
-  error: null,
-  currentAction: null
+	create: {},
+	list: {},
+	isLoading: false,
+	error: null,
+	currentAction: null
 }
 
 export const messagesReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action
+	const { type, payload } = action
 
-  switch (type) {
-    case ACTION_TYPES.MESSAGE_CREATE_START:
-      return { ...state, isLoading: true, error: null, currentAction: 'create' }
-    case ACTION_TYPES.MESSAGE_LIST_START:
-      return { ...state, isLoading: true, error: null, currentAction: 'list' }
+	switch (type) {
+		case ACTION_TYPES.MESSAGE_CREATE_START:
+			return { ...state, isLoading: true, error: null, currentAction: 'create' }
+		case ACTION_TYPES.MESSAGE_LIST_START:
+			return { ...state, isLoading: true, error: null, currentAction: 'list' }
 
-    case ACTION_TYPES.MESSAGE_CREATE_SUCCESS:
-      return { ...state, isLoading: false, create: payload, error: null }
-    case ACTION_TYPES.MESSAGE_LIST_SUCCESS:
-      return { ...state, isLoading: false, list: payload, error: null }
+		case ACTION_TYPES.MESSAGE_CREATE_SUCCESS:
+			return { ...state, isLoading: false, create: payload, error: null }
+		case ACTION_TYPES.MESSAGE_LIST_SUCCESS:
+			return { ...state, isLoading: false, list: payload, error: null }
 
-    case ACTION_TYPES.MESSAGE_CREATE_FAILED:
-      return { ...state, isLoading: false, error: payload, currentAction: 'create' }
-    case ACTION_TYPES.MESSAGE_LIST_FAILED:
-      return { ...state, isLoading: false, error: payload, currentAction: 'list' }
+		case ACTION_TYPES.MESSAGE_CREATE_FAILED:
+			return { ...state, isLoading: false, error: payload, currentAction: 'create' }
+		case ACTION_TYPES.MESSAGE_LIST_FAILED:
+			return { ...state, isLoading: false, error: payload, currentAction: 'list' }
 
-    default:
-      return state
-  }
+		default:
+			return state
+	}
 }
