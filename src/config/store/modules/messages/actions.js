72540692f5d193b6fb41bba91c102925340c7fc9
maybenotpanda <maybenotpanda@gmail.com>
2025-09-19 00:25:29 +0700

    feat-message: changes position read & create

--- src/config/store/modules/messages/actions.js
+++ src/config/store/modules/messages/actions.js
@@ -8,24 +8,6 @@ import apiGeneral from 'config/services/api-general'
 // ** Utils Imports
 import { failureAction, startAction, successAction } from 'config/utils/action'
 
-// ! read
-export const listMessages = ({ page, limit }) => {
-  return async (dispatch) => {
-    dispatch(startAction(ACTION_TYPES.MESSAGES_LIST_START))
-
-    try {
-      const res = await apiGeneral.request({
-        method: 'get',
-        url: `${API_URLS.LIST_MESSAGE}?page=${page}&limit=${limit}`
-      })
-      dispatch(successAction(ACTION_TYPES.MESSAGES_LIST_SUCCESS, res.data.data))
-    } catch (err) {
-      const message = err.response?.data || 'Unknown error occurred'
-      dispatch(failureAction(ACTION_TYPES.MESSAGES_LIST_FAILED, message.message))
-    }
-  }
-}
-
 // ! create
 export const createMessage = (req) => {
   return async (dispatch) => {
@@ -41,6 +23,24 @@ export const createMessage = (req) => {
     } catch (err) {
       const message = err || 'Unknown error occurred'
       dispatch(failureAction(ACTION_TYPES.MESSAGE_CREATE_FAILED, message))
+    }
+  }
+}
+
+// ! read
+export const listMessage = ({ page, limit }) => {
+  return async (dispatch) => {
+    dispatch(startAction(ACTION_TYPES.MESSAGE_LIST_START))
+
+    try {
+      const res = await apiGeneral.request({
+        method: 'get',
+        url: `${API_URLS.LIST_MESSAGE}?page=${page}&limit=${limit}`
+      })
+      dispatch(successAction(ACTION_TYPES.MESSAGE_LIST_SUCCESS, res.data.data))
+    } catch (err) {
+      const message = err.response?.data || 'Unknown error occurred'
+      dispatch(failureAction(ACTION_TYPES.MESSAGE_LIST_FAILED, message.message))
     }
   }
 }
