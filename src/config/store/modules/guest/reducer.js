6a5b2f5ebec7131c497d24da7924bce605c21cb4
maybenotpanda <maybenotpanda@gmail.com>
2025-09-19 00:35:35 +0700

    feat-guest: remove name action & clean code with prettiercc

--- src/config/store/modules/guest/reducer.js
+++ src/config/store/modules/guest/reducer.js
@@ -2,26 +2,25 @@
 import ACTION_TYPES from 'config/enums/types'
 
 const INITIAL_STATE = {
-  detail: {},
-  isLoading: false,
-  error: null,
-  currentAction: null
+	detail: {},
+	isLoading: false,
+	error: null
 }
 
 export const guestReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action
+	const { type, payload } = action
 
-  switch (type) {
-    case ACTION_TYPES.GUEST_DETAIL_START:
-      return { ...state, isLoading: true, error: null, currentAction: 'detail' }
+	switch (type) {
+		case ACTION_TYPES.GUEST_DETAIL_START:
+			return { ...state, isLoading: true, error: null }
 
-    case ACTION_TYPES.GUEST_DETAIL_SUCCESS:
-      return { ...state, isLoading: false, detail: payload, error: null }
+		case ACTION_TYPES.GUEST_DETAIL_SUCCESS:
+			return { ...state, isLoading: false, detail: payload, error: null }
 
-    case ACTION_TYPES.GUEST_DETAIL_FAILED:
-      return { ...state, isLoading: false, error: payload, currentAction: 'detail' }
+		case ACTION_TYPES.GUEST_DETAIL_FAILED:
+			return { ...state, isLoading: false, error: payload }
 
-    default:
-      return state
-  }
+		default:
+			return state
+	}
 }
